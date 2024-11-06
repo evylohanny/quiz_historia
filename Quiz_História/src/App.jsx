924da4bea0b2224f3e04b936df17a181243576f3
evylohanny <evelyn_l_silva@estudante.sesisenai.org.br>
2024-11-06 17:19:39 -0300

    criação de alguns componentes

---
+++ Quiz_História/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h1'>
+        <h1>Quiz</h1>
+      </div>
+      <div class="button-container">
+        <button class="start-button">START</button>
+      </div>
+    </>
+  )
+}
+
+export default App
